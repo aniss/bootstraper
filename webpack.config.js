b978773f2d0b9b88fda7bad9c32c2f35ed612052
Anis Bouraba <anis.bouraba@appsbroker.com>
2015-04-11 13:33:00 +0100

    added hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var RewirePlugin = require('rewire-webpack');
 var NODE_ENV = process.env.NODE_ENV;
-var development = (NODE_ENV === 'development');
+var DEVELOPMENT = (NODE_ENV === 'development');
+
 
 /**********************
  * Base Configuration *
@@ -16,7 +17,8 @@ var config = {
   entry: './js/app.jsx',
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: DEVELOPMENT ? 'http://localhost:4000/' : ''
   }
 };
 
@@ -29,7 +31,7 @@ var config = {
 var loaders = [{
   test: /\.jsx?$/,
   exclude: /node_modules/,
-  loaders: development ? ['react-hot', 'babel'] : ['babel']
+  loaders: DEVELOPMENT ? ['react-hot', 'babel'] : ['babel']
 }, {
   test: /\.s?css$/,
   loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!sass')
@@ -58,26 +60,21 @@ config.resolve = {
  * Plugins *
  ***********/
 
+
 config.plugins = [
   new ExtractTextPlugin('app.[hash].css'),
   new HtmlWebpackPlugin({
     template: path.join(__dirname, 'public/index.html')
   }),
   new webpack.ProvidePlugin({ fetch: 'isomorphic-fetch' }),
-  new webpack.DefinePlugin({ '__DEV__':  (NODE_ENV === 'development')})
+  new webpack.DefinePlugin({ '__DEV__':  DEVELOPMENT}),
+  new RewirePlugin()
 ];
-
-if(development) {
-  config.plugins.concat([
-    new RewirePlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]);
-}
 
 /*********
  * Proxy *
  *********/
+
 
 config.devServer = {
   proxy: {
